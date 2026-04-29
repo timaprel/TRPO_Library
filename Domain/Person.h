47d5bea6bc89cfcdbfda3c72ca5534cac63ad70c
timaprel <timaprel@mail.ru>
2026-04-29 23:00:10 +0300

    Задание 6. Базовая сущность Person

--- Domain/Person.h
+++ Domain/Person.h
@@ -1,16 +1,24 @@
 #pragma once
+#include <string>
 #include <vector>
 #include <memory>
 #include "Role.h"
 
+class Role;
 class Person
 {
 public:
-    explicit Person(int id);
+    Person(int id, std::string fullName, std::string email);
+    int getId() const;
+    const std::string &getFullName() const;
+    const std::string &getEmail() const;
     void addRole(std::shared_ptr<Role> role);
     bool hasRole(const std::string &roleName) const;
+    void changeEmail(const std::string &newEmail);
 
 private:
     int id_;
+    std::string fullName_;
+    std::string email_;
     std::vector<std::shared_ptr<Role>> roles_;
 };
