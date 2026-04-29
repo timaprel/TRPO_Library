8dd24c45d017d0e65fc4716ff6dd01a303250e56
timaprel <117346317+timaprel@users.noreply.github.com>
2026-04-29 22:32:46 +0300

    Задание 6. Базовая сущность Person

--- Domain/Person.cpp
+++ Domain/Person.cpp
@@ -1,7 +1,35 @@
 #include "Person.h"
+#include <stdexcept>
+#include <utility>
 
-Person::Person(int id)
-    : id_(id) {}
+Person::Person(int id, std::string fullName, std::string email)
+    : id_(id),
+      fullName_(std::move(fullName)),
+      email_(std::move(email))
+{
+    if (fullName_.empty())
+        throw std::invalid_argument("Full name is empty");
+}
+
+int Person::getId() const
+{
+    return id_;
+}
+const std::string &Person::getFullName() const
+{
+    return fullName_;
+}
+const std::string &Person::getEmail() const
+{
+    return email_;
+}
+void Person::changeEmail(const std::string &newEmail)
+{
+    if (newEmail.empty())
+        throw std::invalid_argument("Email cannot be empty");
+    email_ = newEmail;
+}
+
 void Person::addRole(std::shared_ptr<Role> role)
 {
     roles_.push_back(role);
--- Domain/Person.h
+++ Domain/Person.h
@@ -2,15 +2,22 @@
 #include <vector>
 #include <memory>
 #include "Role.h"
+#include <string>
 
 class Person
 {
 public:
-    explicit Person(int id);
+    explicit Person(int id, std::string fullName, std::string email);
     void addRole(std::shared_ptr<Role> role);
     bool hasRole(const std::string &roleName) const;
+    int getId() const;
+    const std::string &getFullName() const;
+    const std::string &getEmail() const;
+    void changeEmail(const std::string &newEmail);
 
 private:
     int id_;
     std::vector<std::shared_ptr<Role>> roles_;
+    std::string fullName_;
+    std::string email_;
 };
