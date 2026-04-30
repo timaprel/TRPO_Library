7d2f0465df1cbef122af548eb782e135aca3c51e
timaprel <timaprel@mail.ru>
2026-04-30 11:06:33 +0300

    Задание 12. Добавление статуса аренды

--- Domain/Arenda.cpp
+++ Domain/Arenda.cpp
@@ -3,26 +3,34 @@
 #include "Copy.h"
 #include <stdexcept>
 
-Arenda::Arenda(std::shared_ptr<Person> person,
-               std::shared_ptr<Copy> copy)
+Arenda::Arenda(std::shared_ptr<Person> person, std::shared_ptr<Copy> copy)
     : person_(person),
       copy_(copy),
-      startDate_(std::chrono::system_clock::now())
+      startDate_(std::chrono::system_clock::now()),
+      status_(ArendaStatus::Active)   // статус активен при создании
 {
     if (!person_)
-    {
         throw std::invalid_argument("Person cannot be null!");
-    }
     if (!copy_)
-    {
         throw std::invalid_argument("Copy cannot be null!");
-    }
 }
+
 std::shared_ptr<Person> Arenda::getPerson() const
 {
     return person_;
 }
+
 std::shared_ptr<Copy> Arenda::getCopy() const
 {
     return copy_;
+}
+
+std::chrono::system_clock::time_point Arenda::getStartDate() const
+{
+    return startDate_;
+}
+
+bool Arenda::isActive() const
+{
+    return status_ == ArendaStatus::Active;
 }
--- Domain/Arenda.h
+++ Domain/Arenda.h
@@ -8,14 +8,22 @@ class Copy;
 class Arenda
 {
 public:
-    Arenda(std::shared_ptr<Person> person,
-           std::shared_ptr<Copy> copy);
+    enum class ArendaStatus {
+        Active,
+        Closed
+    };
+
+    Arenda(std::shared_ptr<Person> person, std::shared_ptr<Copy> copy);
+
+    // Геттеры
     std::shared_ptr<Person> getPerson() const;
     std::shared_ptr<Copy> getCopy() const;
+    std::chrono::system_clock::time_point getStartDate() const;
+    bool isActive() const;   // возвращает true, если статус Active
 
 private:
     std::shared_ptr<Person> person_;
     std::shared_ptr<Copy> copy_;
-    std::chrono::system_clock::time_point
-        startDate_;
+    std::chrono::system_clock::time_point startDate_;
+    ArendaStatus status_;
 };
