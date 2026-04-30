4a359e16ca42279b0378e77554780934906abad3
timaprel <timaprel@mail.ru>
2026-04-30 09:51:26 +0300

    Задание 7. Добавление ролей через композицию

--- Domain/Role.h
+++ Domain/Role.h
@@ -8,3 +8,33 @@ public:
     virtual ~Role() = default;
     virtual std::string getName() const = 0;
 };
+
+// Роль "Читатель"
+class ReaderRole : public Role
+{
+public:
+    std::string getName() const override
+    {
+        return "Reader";
+    }
+};
+
+// Роль "Библиотекарь"
+class BibliotekarRole : public Role
+{
+public:
+    std::string getName() const override
+    {
+        return "Bibliotekar";
+    }
+};
+
+// Роль "Администратор"
+class AdministratorRole : public Role
+{
+public:
+    std::string getName() const override
+    {
+        return "Administrator";
+    }
+};
